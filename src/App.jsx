7463579f786b4622bdccec272107b449ff2639ca
rizky <rizkiesws@gmail.com>
2026-04-20 16:00:57 +0700

    add web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; 
-import MainLayout from './components/Layout/MainLayout';
-import AppRoutes from './routes/AppRoutes';
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import MainLayout from "./components/Layout/MainLayout";
+import AppRoutes from "./routes/AppRoutes";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       // Simpan data di memori selama 5 menit.
-      // Jadi pas di-back, data langsung muncul instan dari cache tanpa loading.
-      staleTime: 1000 * 60 * 5, 
-      
-      // (Opsional) Biar pas kamu pindah tab browser, datanya gak tiba-tiba loading ulang
-      refetchOnWindowFocus: false, 
+      // data langsung muncul instan dari cache tanpa loading.
+      staleTime: 1000 * 60 * 5,
+
+      refetchOnWindowFocus: false,
     },
   },
 });
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-      
-        <MainLayout>
-           <AppRoutes />
-        </MainLayout>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <MainLayout>
+            <AppRoutes />
+          </MainLayout>
+        </BrowserRouter>
+      </QueryClientProvider>
+      <Analytics />
+    </>
   );
 }
 
